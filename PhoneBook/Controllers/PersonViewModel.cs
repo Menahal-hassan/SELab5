cf9deb4a3ef5e7cd79d05d192610154d5db5c2aa
Alia <alialiaqat250@gmail.com>
2018-10-10 22:38:51 +0500

    edit page

---
+++ PhoneBook/Controllers/PersonViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace PhoneBook.Controllers
+{
+    public class PersonViewModel
+    {
+        public string FirstName{ get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string TwitterId { get; set; }
+        public string EmailId { get; set; }
+        public string FacebookAccountId { get; set; }
+        public string HomeAddress { get; set; }
+        public string ImagePath { get; set; }
+        public string LinkedId { get; set; }
+        public string HomeCity { get; set; }
+        public bool DateofBirth { get; set; }
+
+       
+
+
+
+    }
+}
---
+++ PhoneBook/Models/contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Models
+{
+    public class contact
+    {
+        public string Contactid { get; set; }
+        public string Contactnumber { get; set; }
+        public bool PersonId { get; set; }
+        public bool type { get; set; }
+    }
+}
